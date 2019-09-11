754792ff842d990b624eea16e1721baa67d1b21e
github username <github email address>
2019-09-11 17:10:53 -0400

    Points styled as well as the Sponsors section, begun work on judges

--- src/Components/Judges.js
+++ src/Components/Judges.js
@@ -5,7 +5,9 @@ class Judges extends React.Component {
   render(){
   return (
     <div className="judges">
-    JUDGES
+      <div className="judge"><img className="judgeImage" src="images/steveJobs1.jpg" /><p>Lorem ipsum dolor sit amet</p></div>
+      <div className="judge"><img className="judgeImage" src="images/steveJobs1.jpg" /><p>Lorem ipsum dolor sit amet</p></div>
+      <div className="judge"><img className="judgeImage" src="images/steveJobs1.jpg" /><p>Lorem ipsum dolor sit amet</p></div>
     </div>
   );
  }
--- src/Components/Sponsors.js
+++ src/Components/Sponsors.js
@@ -5,10 +5,16 @@ class Sponsors extends React.Component {
   render(){
   return (
     <div className="sponsors">
-    SPONSORS
+        <div><img className="sponsorsImage" id="google" src="images/google-ads-launched-the-company-lets-google-adwords-go-824438.png"/></div>
+        <div><img className="sponsorsImage" id="spotify" src="images/spotify-logo-png-transparent-spotify-logo-images-348161.png"/></div>
+        <div><img className="sponsorsImage" id="etsy" src="images/logo-etsy-png-png-image-738724.png"/></div>
+        <div><img className="sponsorsImage" id="mongo" src="images/mongodb-atlas-hits-amazon-web-services-marketplace-499956.png"/></div>
+        <div><img className="sponsorsImage" id="ibm" src="images/ibm-logos-png-images-free-download-ibm-logo-png-876586.png"/></div>
     </div>
   );
  }
 }
 
 export default Sponsors;
+
+
