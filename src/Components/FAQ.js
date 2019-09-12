e53fecd2abe1434b214b7f42b3e14e9883d15b5a
github username <github email address>
2019-09-12 14:41:46 -0400

    FAQ, Judges

--- src/Components/FAQ.js
+++ src/Components/FAQ.js
@@ -5,7 +5,24 @@ class FAQ extends React.Component {
   render(){
   return (
     <div className="faq">
-    FAQ
+        <h1>1. WHAT IS A HACKATHON?</h1>
+        <p>Hackathons have long been staple events in the Technology Industry since the late 1990s. A Hackathon is an intense event that brings together computer programmers like software developers, graphic designers and user interface specialists along with industry process experts and professionals to identify issues and create software solutions, usually within a weekend.</p>
+        
+        <h1>2. WHO SHOULD ATTEND?</h1>
+        <p>AEC Professionals – This also includes Facilities Managers, GIS Specialists, Public Utilities, Policy Makers, and everyone that deals with the built environment. Come with your industry experience and the problems you want to solve. Tech Developers – We need tech ninjas, makers, game programmers, and rebels that code. Come to hack with new hardware and tech all in the name of doing something crazy and fantastic–and helpful. UI/UX Designers – We need people like you to make everything look beautiful, work intuitively, and create a stellar user experience. Great applications require great design</p>
+
+        <h1>3. WHAT DO I BRING?</h1>
+        <p>This is a very hands-on and grassroots event. Bring your laptops, mobile phones, Kinects, Oculus Rifts, Leap Motions, wearable computing devices, to whatever inspires you but above all, your energy and your brain! Don’t worry if you forget something, there will be tons of stuff to do.</p>
+
+        <h1>4. WHAT IS THE GOAL OF THE HACKATHON?</h1>
+        <p>We want to create an environment that embraces new ideas and technology solutions for the AEC Industry. A place where technology designers and developers can come to help the industry, and a safe zone for AEC R&D teams to geek out without having to worry about failure or affecting a company’s bottom line. We know it takes longer than a weekend to build great solutions, but this is ample time to form teams of good people and begin creating great solutions.</p>
+
+        <h1>5. HOW DO I FORM OR JOIN A TEAM AND WHY?</h1>
+        <p>We hope teams at the hackathon will form around problems expressed by participating AEC professionals or the provided hack categories. We do encourage participants to start forming teams before the event. If you would like to start a team or are looking for a team to join, be sure to attend the Friday evening mixer or make an announcement at the opening ceremony of the event on Saturday. You can also propose or join a team via our forum, Meetup Community, Facebook page, and LinkedIn group.</p>
+
+        <h1>6. ARE THERE PRIZES FOR THE HACK CATEGORIES?</h1>
+        <p>We are able to provide cash and software prizes thanks to our sponsors!</p>
+
     </div>
   );
  }
--- src/Components/Judges.js
+++ src/Components/Judges.js
@@ -4,10 +4,14 @@ class Judges extends React.Component {
 
   render(){
   return (
+    <div className="judgeDiv">
+    <h1>Judges</h1>
     <div className="judges">
-      <div className="judge"><img className="judgeImage" src="images/steveJobs1.jpg" /><p>Lorem ipsum dolor sit amet</p></div>
-      <div className="judge"><img className="judgeImage" src="images/steveJobs1.jpg" /><p>Lorem ipsum dolor sit amet</p></div>
-      <div className="judge"><img className="judgeImage" src="images/steveJobs1.jpg" /><p>Lorem ipsum dolor sit amet</p></div>
+      
+      <div className="judge"><img className="judgeImage" src="images/elon_musk.png" /><p className="judgeText">Musk</p></div>
+      <div className="judge"><img className="judgeImage" src="images/macgrubber.png" /><p className="judgeText">Macgrubber</p></div>
+      <div className="judge"><img className="judgeImage" src="images/steve-jobs.png" /><p className="judgeText">Jobs</p></div>
+    </div>
     </div>
   );
  }
